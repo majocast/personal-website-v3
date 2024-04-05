93da3a5ea8e56ce6afc856dc769afeb4082551c7
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2024-04-05 00:19:42 -0700

    integrated client component functionality -> Project.tsx

--- src/app/Module/Project/index.tsx
+++ src/app/Module/Project/index.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./Project.module.css";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
-import { IconType } from "react-icons";
+import type { IconType } from "react-icons";
+import LozanoPower from '../../../assets/lozanopowerelectric - Copy.jpg'
+import {
+  BiLogoReact, 
+  BiLogoCss3, 
+  BiLogoHtml5, 
+  BiLogoJavascript,
+} from 'react-icons/bi';
 
 type Image = {
 	src: string | StaticImport;
@@ -14,33 +23,47 @@ type ProjectData = {
 	link: string;
 	image: Image;
 	description: string;
-	technologies: IconType[];
+	technologies: string[];
 }
 
 const ImageStyle = {
-	height: 'auto',
-	width: '100%',
+  height: '100%',
+  width: '100%'
 }
 
 export default function Project(projectData: ProjectData): ReactElement {
-	const technologies: ReactElement[] = projectData.technologies.map((tech: IconType, i: number): ReactElement => {
+	/*const technologies: ReactElement[] = projectData.technologies.map((tech: string, i: number): ReactElement => {
 		const Icon = tech;
+		return <span style={{color: "white"}}>{tech}</span>
+		//return <Icon key={i} size={35} color="white"/>
+	})*/
+
+	//TODO: integrate method to grab strings and import on demand for IconTypes
+	const technologies = [BiLogoHtml5, BiLogoCss3, BiLogoReact, BiLogoJavascript]
+	const processedTechnologies: ReactElement[] = technologies.map((tech: IconType, i: number): ReactElement => {
+		const Icon = tech;
+		//return <span style={{color: "white"}}>{tech}</span>
 		return <Icon key={i} size={35} color="white"/>
 	})
 
-	return <div className={styles.ProjectContainer}>
-		<div className={styles.ProjectContent}>
-			<section>
-				<Image loading="lazy" style={ImageStyle} src={projectData.image.src} alt={projectData.image.alt}/>
-			</section>
-			<section className={styles.ProjectText}>
-				<div className={styles.ProjectTechnologies}>
-					{technologies}
-				</div>
-				<div>
-					{projectData.description}
-				</div>
-			</section>
+	let projectImg = projectData.image.src !== "" ? projectData.image.src : LozanoPower;
+	
+	return (
+		<div className={styles.ProjectContainer}>
+			<div className={styles.ProjectContent}>
+				<button>
+						<Image loading="lazy" style={ImageStyle} src={projectImg} alt={projectData.image.alt}/>
+				</button>
+				<section className={styles.ProjectText}>
+						<div className={styles.ProjectTechnologies}>
+							{processedTechnologies}
+						</div>
+						<div>
+							{projectData.description}
+						</div>
+				</section>
+			</div>
 		</div>
-	</div>
+	)
+	
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+"use server"
+
 import styles from "./page.module.css";
-import { lazy } from "react";
 import Project from './Module/Project'
 import LozanoPower from '../assets/lozanopowerelectric - Copy.jpg'
 import {
@@ -8,20 +9,21 @@ import {
   BiLogoHtml5, 
   BiLogoJavascript,
 } from 'react-icons/bi';
+import { ReactElement } from "react";
 //const Project = lazy(() => import('./Module/Project/index'))
 
 const MockProjectData = {
   name: "Lozano Power Electric",
 	image: {
-    src: LozanoPower,
+    src: "",
     alt: "Hello there test"
   },
   link: "https://www.lozanopowerelectric.com/",
 	description: "Modern and unique responsive company website created to showcase the work, commitment, and services of Lozano Power Electric. Process included Figma drafting, quality assurance and verification between myself and the founder of the company, and customer centric design.",
-	technologies: [BiLogoHtml5, BiLogoCss3, BiLogoReact, BiLogoJavascript]
+	technologies: ["BiLogoHtml5", "BiLogoCss3", "BiLogoReact", "BiLogoJavascript"]
 }
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className={styles.main}>
       <Project {...MockProjectData}/>
